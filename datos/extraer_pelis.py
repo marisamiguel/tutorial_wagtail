9060205c1d650c74e8c40b736bae8c24c8ea2e04
lmorillas <morillas@gmail.com>
2022-01-25 19:04:44 +0100

    ejercicio peliculas

--- datos/extraer_pelis.py
+++ datos/extraer_pelis.py
@@ -1,8 +1,39 @@
 '''
 Programa que extrae los datos de https://www.imdb.com/chart/top/
-y genera un json ...
+y genera un json con los datos de las películas
 
+Trabajo: 
+* Inspeccionar la estructura de la web
+* Extraer los datos de las películas con xpath
 '''
+
+import requests
+from lxml import html
+from urllib.parse import urljoin
+import json
+
+headers = {"Accept-Language": "es-es,es;q=0.5"}
+
+
+
+def detalle(url_peli):
+    url = urljoin ('https://www.imdb.com', url_peli)
+    
+    response = requests.get(url, headers=headers)
+    pagina = html.fromstring(response.text)
+    script = pagina.xpath('//script[@type="application/ld+json"]')[0]
+    datos = json.loads(script.text)
+
+    # parental
+    metadatos = pagina.xpath('//ul[contains(@class, "TitleBlockMetaData")]/li')
+
+    parental = metadatos[1].xpath('.//a[contains(@href, "parentalguide/certificates")]')[0].text
+    datos['parental'] = parental
+    
+    duracion = metadatos[2].text_content()
+    datos['duracion'] = duracion
+
+    return datos
 
 
 def datos_peli(peli):
@@ -17,6 +48,7 @@ def datos_peli(peli):
     elementos = peli.xpath(".//td")
     imagen = elementos[0]
     titulo = elementos[1]
+    rating = elementos[2]
 
     #imagen
     imagensrc = imagen.xpath(".//img/@src")[0]
@@ -37,5 +69,31 @@ def datos_peli(peli):
     # año
     year = titulo.xpath(".//span/text()")[0].replace("(", "").replace(')', '')
     datos['year'] = year
+
+    # ranking 
+    pospunto = titulo.text_content().find('.')
+    ranking = titulo.text_content()[:pospunto].strip()
+    datos['ranking'] = ranking
+
+    # rating 
+    rating_ = rating.xpath(".//strong/text()")[0]
+    datos['rating'] = rating_
     
+    datos.update(detalle(url))
+
     return datos
+
+if __name__ == '__main__':
+
+    url = 'https://www.imdb.com/chart/top/'
+    
+    response = requests.get(url, headers=headers)
+    pagina = html.fromstring(response.text)
+
+    peliculas = pagina.xpath("//table[@data-caller-name='chart-top250movie']/tbody/tr")
+
+    # test
+    assert(len(peliculas) == 250)
+
+    datos = [datos_peli(p) for p in peliculas]
+    json.dump(datos, open('datos_pelis_plus.json', 'w'))
